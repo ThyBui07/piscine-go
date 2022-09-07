168bfbbca70286614ad9c1ea2d35f38414d102a4
ThyBui07 <buithinguyenthy@gmail.com>
2022-09-07 19:37:50 +0300

    swapbit

--- main.go
+++ main.go
@@ -1,42 +1,13 @@
 package main
 
-import (
-	"os"
+import "fmt"
 
-	"github.com/01-edu/z01"
-)
-
-func main() {
-	args := os.Args[1:]
-	str := ""
-	if len(args) < 2 {
-		z01.PrintRune('\n')
-	} else {
-		str = args[0] + args[1]
-		str = RemoveDup(str)
-		for _, x := range str {
-			z01.PrintRune(x)
-		}
-	}
-	z01.PrintRune('\n')
+func SwapBits(octet byte) byte {
+	return octet<<4 | octet>>4
 }
 
-func RemoveDup(s string) string {
-	res := ""
-
-	for _, x := range s {
-		var dup bool
-		for _, y := range res {
-			if x != y {
-				dup = false
-			} else {
-				dup = true
-				break
-			}
-		}
-		if dup == false {
-			res += string(x)
-		}
-	}
-	return res
+func main() {
+	var num byte = 65
+	fmt.Printf("%08b\n", num)
+	fmt.Printf("%08b\n", SwapBits(num))
 }
